d984c962ac23af35225426be93edf17a941ef2b0
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 14:01:55 +0530

    Create collatz sequence.py

---
+++ collatz sequence.py
@@ -0,0 +1,16 @@
+...
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+Print the numbers in the sequence and also print the number of times the rule has to be applied in order to reach 1.
+...
+Sample Input:
+5
+Sample Output:
+5
+16
+8
+4
+2
+1
+5
