d3825fb3f000f68f0a9c2f6beb96aecfd3188154
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:50:51 +0530

    Create fruitseller.py

---
+++ fruitseller.py
@@ -0,0 +1,13 @@
+...
+Input format:
+Input consists of 2 floating point numbers
+The first input corresponds to the total cost(X)
+The second input corresponds to the sold cost(Y)
+Output format:
+Print "Profit or Loss" with Rupees.
+...
+Sample Input:
+60
+4
+Sample Output:
+Loss : Rs.12.00
