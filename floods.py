9db9a3f13599e4ed695994dbc7c596098cbb243a
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:40:46 +0530

    Create floods.py

---
+++ floods.py
@@ -0,0 +1,14 @@
+...
+First line of the input is an integer “X” that specifies the salary of the senior players in rupees.
+Second line is an integer “Y” that specifies the salary of the junior players in rupees.
+Output format:
+Output should display a flood that gives the total contribution of money in rupees from the cricket team. The float value should be displayed correct to 2 decimal places.
+Sample input and output 1:
+45000
+40000
+215000.00
+Sample input and output 2:
+78000
+60000
+354000.00
+...
