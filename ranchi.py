f77fa8baf056f5a9573b7964b5586d9fd520ea37
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:32:26 +0530

    Create ranchi.py

---
+++ ranchi.py
@@ -0,0 +1,28 @@
+...
+Team 1:
+Team Name:
+DAV Jawahar Vidhya Mandir
+Score:
+150
+Overs played:
+20
+Team 2:
+Team name:
+Kendriya School
+Score:
+110
+Overs played:
+18
+Match Details:
+Team 1:
+Name: DAV Jawahar Vidhya Mandir
+Score: 150
+Overs played: 20
+Team 2:
+Name:  Kendriya School
+Score: 110
+Overs played: 18
+...
+cAge=int(input())
+deviAge =((cAge+3)*4)-3
+print(deviAge)
