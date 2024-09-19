2a9adf344ec5eafac5b84600def84ca71c2a0e33
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:54:37 +0530

    Create acetraining.py

---
+++ acetraining.py
@@ -0,0 +1,18 @@
+...
+Input format:
+Input consists of 3 integers and a string
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the lab which is allocated for ACE training
+Output format:
+Print the minimal seating capacity lab amongst the available labs.
+Refer the Sample output for formatting
+  ...
+Sample Input:
+30
+40
+20
+L3
+Sample Output:
+L1
