cd7f72b82849e4590fed2d2e1189d241a6ce900b
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:49:06 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,12 @@
+...
+Input format:
+The input consist of a character
+Output format:
+The output consists of a below-given string
+“Vowel” / “Consonant” / “Not an alphabet”
+...
+Sample Input:
+e
+
+Sample Output:
+Vowel
