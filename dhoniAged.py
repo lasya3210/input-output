98400163465271535e86d0a3234c176cc99f774a
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:20:18 +0530

    Update dhoniAged.py

--- dhoniAged.py
+++ dhoniAged.py
@@ -9,47 +9,4 @@ Enter the age:
 35
 Age of Dhoni is 35
 ...
-  a=int(input())
-print("Age of Dhoni is",a)
-   ...
-   Refer Sample input and output for formatting specifications.
-All text in bold corresponds to input and the rest corresponds to output.
-Sample Input and Output:
-Banerjee's Question:
-Will you be a wicketkeeper?
-Dhoni's Reply:
-I will if i get a chance.
-For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply was "I will if I get a chance."
-...
-  p=input("Banerjee's Question:\n")
-r=input("Dhoni's Reply:\n")
- ...
- Team 1:
-Team Name:
-DAV Jawahar Vidhya Mandir
-Score:
-150
-Overs played:
-20
-Team 2:
-Team name:
-Kendriya School
-Score:
-110
-Overs played:
-18
-Match Details:
-Team 1:
-Name: DAV Jawahar Vidhya Mandir
-Score: 150
-Overs played: 20
-Team 2:
-Name:  Kendriya School
-Score: 110
-Overs played: 18
-...
- cAge=int(input())
-deviAge =((cAge+3)*4)-3
-print(deviAge)
-
-
+  
