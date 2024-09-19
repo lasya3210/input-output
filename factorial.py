2e0b5fa51fc0281e18159541059b8c39d5737b06
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 14:00:03 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+...
+Input format:
+The input containing an integer 'n' which denotes the given number.
+Output format:
+If the given number is factorial, print "Yes". Otherwise, print "No".
+...
+Sample Input:
+6
+Sample Output:
+Yes
