9e52d08e973c43b94012b5f0e30ae83d4f58bd93
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 14:03:03 +0530

    Create trendynumber.py

---
+++ trendynumber.py
@@ -0,0 +1,10 @@
+...
+Input format:
+The input containing an integer 'n' which denotes the given number
+Output format:
+If the given number is a trendy number, then print "Trendy Number". Otherwise, print "Not a Trendy Number".
+...
+Sample Input:
+791
+Sample Output:
+Trendy Number
