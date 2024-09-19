741a531ca451278e22fb4a24dce831b43898e874
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:41:26 +0530

    Create indian.squad.py

---
+++ indian.squad.py
@@ -0,0 +1,13 @@
+First line of the input is an integer that corresponds to the total age of the 3 players.
+Output format:
+Output should display the ages of the three players in 3 lines. The age of the eldest player should be displayed in the last line.
+Sample input and output 1:
+70
+20
+20
+30
+Sample input and output 2:
+100
+30
+30
+40
