6b8db9246b5bfb9fe5d06a79f7d30d8ccc1090c8
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:47:49 +0530

    Create integers.py

---
+++ integers.py
@@ -0,0 +1,13 @@
+...
+Input format:
+Input consist of 2 integers
+The first input corresponds to  the first number.(a)
+The second input corresponds to the second number.(b)
+Output format:
+If the first number is equal to the second number, print "x and y are equal". Otherwise, print "x greater than y" or "x less than y"
+...
+Sample Input:
+6
+8
+Sample Output:
+6 less than 8
