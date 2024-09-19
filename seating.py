f237d95dd7e342250fc80e7f2c31494b70a3d7bc
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:56:21 +0530

    Create seating.py

---
+++ seating.py
@@ -0,0 +1,18 @@
+...
+Input format:
+Input consists of 4 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the number of students(x)
+Output format:
+Print the number of labs which can accommodate the 'n' number of students
+Refer the Sample output for formatting
+...
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+2 
