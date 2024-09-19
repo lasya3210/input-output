c6257b02d3c26363dbee23c90b6806979c1f487f
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:39:54 +0530

    Create extra bonus.py

---
+++ extra bonus.py
@@ -0,0 +1,23 @@
+...
+Input format:
+First line of the input contains an integer that corresponds to the number of No-balls.
+Second line of the input contain an integer that corresponds to the numbers of wides.
+Third line of the input contains an integer that corresponds to the number of byes.
+Fourth line of the input contain an integer that corresponds to the numbers of led-byes.
+Fifth line of the input contains an integer that corresponds to the numbers of penalty runs.
+Output format:
+Output should display an integer that returns the total runs that the extras together contribute to team’s total.
+Sample input and output 1:
+4
+7
+3
+10
+3
+39
+Sample input and output 2:
+2
+3
+7
+1
+17
+...
