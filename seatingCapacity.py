c3d22f221a573fd2bdee96ba23dd08036915dd1c
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:53:32 +0530

    Create seatingCapacity.py

---
+++ seatingCapacity.py
@@ -0,0 +1,16 @@
+...
+Input format:
+Input consists of 3 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+Output format:
+Print the minimal seating lab capacity
+Refer the Sample output for formatting
+...
+Sample Input:
+30
+40
+20
+Sample Output:
+L3
