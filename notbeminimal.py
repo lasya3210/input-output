4865525832b422d8707633ba8819ade90ccd643e
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:57:35 +0530

    Create notbeminimal.py

---
+++ notbeminimal.py
@@ -0,0 +1,18 @@
+...
+Input format:
+Input consists of 4 integers
+The first input denotes 'x'
+The second input denotes 'y'
+The third input denotes 'z'
+The fourth input denotes 'n'
+Output format:
+Print the lab which is suitable for  'n' number of students
+Refer the Sample output for formatting
+...
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+L1
