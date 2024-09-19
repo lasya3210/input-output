2320ddc802275ebc91bcb09b25b4bc66913f48b1
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:42:20 +0530

    Create principalamount.py

---
+++ principalamount.py
@@ -0,0 +1,24 @@
+Input Format:
+The first line contains the principal amount (principal).
+The second line contains the rate of interest (rate) per annum.
+The third line contains the time (time) in years.
+Output Format:
+A single line containing the simple interest calculated.
+Sample Input 1
+1000
+5
+2
+Sample Output 1
+100.0
+Sample Input 2
+5000
+8.5
+3
+Sample Output 2
+1275.0
+Sample Input 3
+5000
+8.5
+3
+Sample Output 3
+525.0
