e12c5c127a7786b894537b9f92df46e72c27331f
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:52:33 +0530

    Create currentage.py

---
+++ currentage.py
@@ -0,0 +1,14 @@
+...
+Input format:
+Input consists of 2 integers
+The first integer corresponds to the last 2 digits of the birth year
+The second integer corresponds to the last 2 digits of the current year
+Output format:
+Print the user's current age
+Refer below sample output for formatting.
+...
+Sample Input:
+62
+00
+Sample Output:
+38
