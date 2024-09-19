af891647a4186be18fb4e9d398123425bfd882be
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:51:50 +0530

    Create fees.py

---
+++ fees.py
@@ -0,0 +1,15 @@
+...
+Input format:
+The first input corresponds to the student type
+The second input corresponds to the tuition fee
+The third input corresponds to the bus fee or hostel fees
+Output format:
+Print the total fee amount of the corresponding student with 2 decimal places.
+Refer below sample output for formatting
+...
+Sample Input:
+MSH
+40000
+50000
+Sample Output:
+90000.00
