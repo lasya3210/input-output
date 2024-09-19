2159cc7f7f874c3dc2543f356e8e585ac56ca0b1
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:30:12 +0530

    Create banerjee.py

---
+++ banerjee.py
@@ -0,0 +1,10 @@
+...
+Banerjee's Question:
+Will you be a wicketkeeper?
+Dhoni's Reply:
+I will if i get a chance.
+For Banerjee's question "Will you be a wicketkeeper?" Dhoni's confident reply was "I will if I get a chance."
+p=input("Banerjee's Question:\n")
+r=input("Dhoni's Reply:\n")
+print("For Banerjee's Question \"{}\" Dhoni's Confident reply was \"{}\"".format(p,r))
+...
