19b548ecd46c11401c8dc47860a6872f020dc046
lasya3210 <22hr1a3210@mtieat.org>
2024-09-19 13:50:02 +0530

    Create grading.py

---
+++ grading.py
@@ -0,0 +1,10 @@
+...
+Input format:
+The input consists of one integer which corresponds to the marks scored by the student
+
+Output format:
+If a student marks greater than 100, print "Invalid Input". Otherwise, print the grade.
+...
+Sample Input:
+78
+Sample Output:C
